75534f2c9d9eaeb98295ae4118db84029a0cb908
404allen404 <n26120935@gs.ncku.edu.tw>
2025-11-29 15:27:16 +0800

    Update config.mk for internal usage

--- config.mk
+++ config.mk
@@ -1,20 +1,8 @@
 WARN=-Wall -Wextra -Wno-unused-function -Wno-unused-parameter
 
 # append -DUSE_PERF_EVENT to CFLAGS, if the cycle csr isn't exposed
-# try -DUSE_PERF_EVENT_SLOW if the abvoe doesn't work
+# try -DUSE_PERF_EVENT_SLOW if the above doesn't work
 
-# freestanding using any recent clang build
 CC=clang
-CFLAGS=--target=riscv64 -march=rv64gv -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
-#CFLAGS=--target=riscv32 -march=rv32gc_zve32f_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
-
-
-# full cross compilation toolchain
-#CC=riscv64-linux-gnu-gcc
-#CFLAGS=-march=rv64gcv -O3 ${WARN}
-
-# native build
-#CC=cc
-#CFLAGS=-march=rv64gcv -O3 ${WARN}
-
+CFLAGS=--target=riscv64-unknown-elf -march=rv64ima_zve64x -O3 ${WARN} --gcc-toolchain=${RISCV} --sysroot=${RISCV}/riscv64-unknown-elf
 
